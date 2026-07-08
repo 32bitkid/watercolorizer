304798fea839d95c14f2e9aac79aa38eb14e3446
J Holmes <32bitkid@gmail.com>
2026-07-08 11:45:06 -0600

    Chore: use Math constants instead

--- libs/watercolorizer/src/winding-order.spec.ts
+++ libs/watercolorizer/src/winding-order.spec.ts
@@ -4,13 +4,13 @@ describe('windingOrderOf()', () => {
   it('should detect clockwise winding', () => {
     const cwOctagon: [number, number][] = [
       [0, 1],
-      [0.70711, 0.70711],
+      [Math.SQRT1_2, Math.SQRT1_2],
       [1, 0],
-      [0.70711, -0.70711],
+      [Math.SQRT1_2, -Math.SQRT1_2],
       [0, -1],
-      [-0.70711, -0.70711],
+      [-Math.SQRT1_2, -Math.SQRT1_2],
       [-1, 0],
-      [-0.70711, 0.70711],
+      [-Math.SQRT1_2, Math.SQRT1_2],
     ];
     expect(windingOrderOf(cwOctagon, true)).toBe('cw');
   });
@@ -18,13 +18,13 @@ describe('windingOrderOf()', () => {
   it('should detect counter-clockwise winding', () => {
     const ccwOctagon: [number, number][] = [
       [0, 1],
-      [-0.70711, 0.70711],
+      [-Math.SQRT1_2, Math.SQRT1_2],
       [-1, 0],
-      [-0.70711, -0.70711],
+      [-Math.SQRT1_2, -Math.SQRT1_2],
       [0, -1],
-      [0.70711, -0.70711],
+      [Math.SQRT1_2, -Math.SQRT1_2],
       [1, 0],
-      [0.70711, 0.70711],
+      [Math.SQRT1_2, Math.SQRT1_2],
     ];
     expect(windingOrderOf(ccwOctagon, true)).toBe('ccw');
   });
